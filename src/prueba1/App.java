b86486422244319f409201b78744525215cc9f7c
unknown <clariespertino62@gmail.com>
2021-05-12 20:27:19 -0300

    hecho

---
+++ src/prueba1/App.java
@@ -0,0 +1,13 @@
+package prueba1;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t = new Test();
+		Integer x=22;
+		t.hacerAlgo(x);
+		System.out.println(x);
+	}
+
+}
---
+++ src/prueba1/Test.java
@@ -0,0 +1,8 @@
+package prueba1;
+
+public class Test {
+	private Integer y=33;
+	public void hacerAlgo(Integer parx) {
+		parx=parx+this.y;
+	}
+}
